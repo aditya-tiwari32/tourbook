e8c356d5cb69a9ad84f32a5dc4bdebb12533fac1
Aditya <aditiwari32@gmail.com>
2022-06-05 20:32:40 +0530

    GEO Location python

---
+++ Geo/where.js
@@ -0,0 +1,13 @@
+myData = [
+[19.1334302,72.9132679, 'Main Gate Rd, IIT Area, Powai, Mumbai, Maharashtra 400076, India'],
+[28.545718,77.19276789999999, 'IIT Delhi Main Rd, IIT Campus, Hauz Khas, New Delhi, Delhi 110016, India'],
+[18.4206487,73.90483040000001, 'Near Bopdev Ghat, Kondhwa Annexe, Yewalewadi, Pune, Maharashtra 411048, India'],
+[22.5737618,88.4176523, 'HC Block,HC7, HC Block, Sector III, Bidhannagar, Kolkata, West Bengal 700106, India'],
+[23.0316882,72.5345322, 'I I M, Vastrapur, Ahmedabad, Gujarat 380015, India'],
+[22.625626,75.790964, 'Prabandh Shikhar, Rau - Pithampur Rd, Indore, Madhya Pradesh 453556, India'],
+[28.5671883,77.2100457, 'Sri Aurobindo Marg, Ansari Nagar, Ansari Nagar East, New Delhi, Delhi 110029, India'],
+[24.8607343,67.0011364, 'Karachi, Karachi City, Sindh, Pakistan'],
+[31.1471305,75.34121789999999, 'Punjab, India'],
+[25.2048493,55.2707828, 'Dubai - United Arab Emirates'],
+[61.52401,105.318756, 'Russia']
+];
